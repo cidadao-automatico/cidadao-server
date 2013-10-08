5511b0ec34e449ca85a671d063ad961563b8228b
tavoaqp <tavoaqp@gmail.com>
2013-10-08 12:58:57 -0300

    Just checkout code on server, you have to manually restart server!

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :scm_verbose, false
 set :keep_releases, 2
 
 set :user, 'ubuntu'
-set :use_sudo, true
+set :use_sudo, false
 set :confirm_stages, "production"
 
 set :rvm_type, :system
@@ -26,49 +26,11 @@ role :app, "julgamento.clipesebandas.com.br"                          # This may
 role :db,  "julgamento.clipesebandas.com.br", :primary => true # This is where Rails migrations will run
 
 # if you want to clean up old releases on each deploy uncomment this:
-after "deploy:update_code", "deploy:update_shared_symlinks"
-after "deploy:restart", "deploy:cleanup"
-after "deploy:cleanup", "npm:install"
-after "npm:install", "bower:install"
-after "bower:install", "grunt:build"
+after "deploy:update_code", "deploy:cleanup"
+#after "deploy:cleanup", "play:run"
 
-
-namespace :deploy do
-  task :update_shared_symlinks do    
-    run "ln -s #{File.join(deploy_to, "shared/node_modules")} #{File.join(release_path, "/frontend-0.2/node_modules")}"
-    run "ln -s #{File.join(deploy_to, "shared/bower_components")} #{File.join(release_path, "/frontend-0.2/app/bower_components")}"    
-  end
-end
-
-namespace :npm do
-  desc 'Install npm packages'
-  task :install, :roles => :web do
-    run "cd #{deploy_to}/current/#{current_frontend} && rvmsudo npm install"
-  end
-end
-
-namespace :bower do
-  desc 'Install bower components'
-  task :install, :roles => :web do
-    run "cd #{deploy_to}/current/#{current_frontend} && bower install"
-  end
-end
-
-namespace :grunt do
-  desc 'Build javascript code using grunt'
-  task :build, :roles => :web do
-    run "cd #{deploy_to}/current/#{current_frontend} && grunt build"
-  end
-end
-#grunt grunt-cli connect-livereload load-grunt-tasks
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+# namespace :play do
+#   task :run do    
+#     run "cd #{release_path} ; play run"
 #   end
 # end
